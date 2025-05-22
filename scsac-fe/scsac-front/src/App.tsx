35a88e90ac8edac342186af83b82fe8559bc90b5
jeongwoo-right <jeongwoocode@gmail.com>
2025-05-22 17:19:43 +0900

    init fe

---
+++ scsac-fe/scsac-front/src/App.tsx
@@ -0,0 +1,14 @@
+// 📁 src/App.tsx
+import Header from './components/Header'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ scsac-fe/scsac-front/src/store/userSlice.ts
@@ -0,0 +1,48 @@
+// src/store/userSlice.ts
+import { createSlice } from '@reduxjs/toolkit'
+import type {PayloadAction} from '@reduxjs/toolkit'
+
+export type UserState = {
+  isLoggedIn: boolean
+  id: number
+  password: string
+  authority: number
+  generation: number
+  affiliate: string
+  name: string | null
+  nickname: string | null
+  boj_id: string | null
+}
+
+// 초기 상태
+const initialState: UserState = {
+  isLoggedIn: false,
+  id: 0,
+  password: '',
+  authority: 3,
+  generation: 0,
+  affiliate: '',
+  name: null,
+  nickname: null,
+  boj_id: null,
+}
+
+// ✅ Payload에는 isLoggedIn을 제외한 나머지 정보만 받도록
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (_, action: PayloadAction<Omit<UserState, 'isLoggedIn'>>) => {
+      return {
+        isLoggedIn: true,
+        ...action.payload,
+      }
+    },
+    logout: () => {
+      return { ...initialState }
+    },
+  },
+})
+
+export const { login, logout } = userSlice.actions
+export default userSlice.reducer
