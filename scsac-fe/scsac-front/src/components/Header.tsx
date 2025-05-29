8df03a4d422be2083172167b02fb55f1c9312505
SNP0301 <studiosnp0301@gmail.com>
2025-05-29 14:17:55 +0900

    [CSS] AdminPage, EditProfilePage, MyPage

--- scsac-fe/scsac-front/src/components/Header.tsx
+++ scsac-fe/scsac-front/src/components/Header.tsx
@@ -4,9 +4,14 @@ import type { RootState } from '../store'
 import { logout } from '../store/userSlice'
 import './Header.css'
 import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 
 
 function Header() {
+  const location = useLocation();
+
+  // 현재 경로가 문제 목록 페이지인지 확인
+  const isProblemList = location.pathname.startsWith('/problems');
   const { isLoggedIn, nickname } = useSelector((state: RootState) => state.user)
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -18,18 +23,20 @@ function Header() {
  }
 
   return (
-    <header className="header">
+    <header className={`header ${isProblemList ? 'problem-list' : 'other'}`}>
       <div className="header-title" 
   style={{ cursor: 'pointer' }} 
   onClick={() => navigate('/problems')}>SCSAC: Algorithm</div>
 
       <div className="header-buttons">
-        {isLoggedIn ? (
-          <>
-            <span className="nickname">{nickname}님</span>
-            <button onClick={() => navigate('/mypage')}>마이페이지</button>
-            <button onClick={handleLogout}>로그아웃</button>
-          </>
+        {isLoggedIn ? ( 
+          <div className="header-user">
+            <div className="nickname">{nickname} 님</div>
+            <div className="header-buttons-vertical">
+              <button onClick={() => navigate('/mypage')}>마이페이지</button>
+              <button onClick={handleLogout}>로그아웃</button>
+            </div>
+          </div>
         ) : (
           <button onClick={() => navigate('/login')}>로그인</button>
         )}
